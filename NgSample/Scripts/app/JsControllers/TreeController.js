5d206c98a86e59560abfad508d0d21c0ff7b6bba
Panayotis Trapatsas <trapatsas@gmail.com>
2014-04-21 12:50:03 +0300

    Tree fixed

--- NgSample/Scripts/app/JsControllers/TreeController.js
+++ NgSample/Scripts/app/JsControllers/TreeController.js
@@ -1,48 +1,48 @@
 ﻿// Tree
 angular.module('main')
 .controller('TreeController', ['$scope', function ($scope) {
-    $scope.treeClass = {
-        treenode: "RootNode",
-        childnodes: [
-          {
-              treenode: "aooooo",
-              childnodes: [
-                {
-                    treenode: "sooooo", childnodes: [{ treenode: "dooooo" }, { treenode: "fooooo" }]
-                },
-                {
-                    treenode: "vooooo", childnodes: [{ treenode: "gooooo" }]
-                }
-              ]
-          },
-          {
-              treenode: "zooooo", childnodes: [{ treenode: "xooooo" }]
-          }
-        ]
+
+    var treeClass = function (val) {
+        this.treenode = val || null;
+        this.right = null;
+        this.left = null;
+        this.childnodes = [this.left, this.right];
     };
 
-    //$scope.treeClass = function (val) {
-    //    this.treenode = val || null;
-    //    this.right = null;
-    //    this.left = null;
-    //    this.childnodes = [this.left, this.right];
-    //};
 
-    //$scope.treeClass.prototype.addnode = function (value) {
-    //    if (!this.value) this.value = value;
-    //    if (value > this.value) {
-    //        if (this.right) {
-    //            this.right.addnode(value);
-    //        } else if (!this.right) {
-    //            this.right = new treeClass(value);
-    //        }
-    //    } else if (value < this.value) {
-    //        if (this.left) {
-    //            this.left.addnode(value);
-    //        } else if (!this.left) {
-    //            this.left = new treeClass(value);
-    //        }
-    //    }
-    //};
+    treeClass.prototype.addnode = function (value) {
+        if (!this.treenode) this.treenode = value;
+        if (value > this.treenode) {
+            if (this.right) {
+                this.right.addnode(value);
+            } else if (!this.right) {
+                this.right = new treeClass(value);
+                this.childnodes[1] = this.right;
+            }
+        } else if (value < this.treenode) {
+            if (this.left) {
+                this.left.addnode(value);
+            } else if (!this.left) {
+                this.left = new treeClass(value);
+                this.childnodes[0] = this.left;
+            }
+        }
+    };
+
+    $scope.add = function (val) {
+        mytree.addnode(val);
+        $scope.data = mytree;
+    };
+
+    var mytree = new treeClass(10);
+
+    $scope.data = mytree;
+    $scope.add(1);
+    $scope.add(12);
+    $scope.add(15);
+    $scope.add(-5);
+    $scope.add(7);
+    $scope.add(11);
+
 
 }]);
