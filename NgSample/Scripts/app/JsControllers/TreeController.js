c89c665687538d0d1ebe7a00302e32ae86252211
Panayotis Trapatsas <trapatsas@gmail.com>
2014-04-21 13:26:15 +0300

    Some more tree fixes

--- NgSample/Scripts/app/JsControllers/TreeController.js
+++ NgSample/Scripts/app/JsControllers/TreeController.js
@@ -3,26 +3,25 @@ angular.module('main')
 .controller('TreeController', ['$scope', function ($scope) {
 
     var treeClass = function (val) {
-        this.treenode = val || null;
+        this.treenode = val.toString() || null;
         this.right = null;
         this.left = null;
         this.childnodes = [this.left, this.right];
     };
 
-
     treeClass.prototype.addnode = function (value) {
-        if (!this.treenode) this.treenode = value;
+        if (this.treenode === null) this.treenode = value;
         if (value > this.treenode) {
-            if (this.right) {
+            if (this.right !== null) {
                 this.right.addnode(value);
-            } else if (!this.right) {
+            } else if (this.right === null) {
                 this.right = new treeClass(value);
                 this.childnodes[1] = this.right;
             }
         } else if (value < this.treenode) {
-            if (this.left) {
+            if (this.left !== null) {
                 this.left.addnode(value);
-            } else if (!this.left) {
+            } else if (this.left === null) {
                 this.left = new treeClass(value);
                 this.childnodes[0] = this.left;
             }
@@ -30,13 +29,11 @@ angular.module('main')
     };
 
     $scope.add = function (val) {
-        mytree.addnode(val);
-        $scope.data = mytree;
+        $scope.data.addnode(val);
     };
 
-    var mytree = new treeClass(10);
+    $scope.data = new treeClass(10);
 
-    $scope.data = mytree;
     $scope.add(1);
     $scope.add(12);
     $scope.add(15);
