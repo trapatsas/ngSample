e681bb2835851c2f686514b89d497096307cd9e8
Panayotis Trapatsas <trapatsas@gmail.com>
2014-04-21 15:04:54 +0300

    Added new Ang App & CORS packet

---
+++ NgSample/Scripts/auth/mainCtrl.js
@@ -0,0 +1,46 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'mainCtrl';
+    angular.module('app').controller(controllerId,
+        ['userAccountService', mainCtrl]);
+    function mainCtrl(userAccountService) {
+        // Using 'Controller As' syntax, so we assign this to the vm variable (for viewmodel).
+        var vm = this;
+        // Bindable properties and functions are placed on vm.
+        vm.title = 'mainCtrl';
+        vm.isRegistered = false;
+        vm.isLoggedIn = false;
+        vm.userData = {
+            userName: "",
+            password: "",
+            confirmPassword: "",
+        };
+        vm.registerUser = registerUser;
+        vm.loginUser = loginUser;
+        vm.getValues = getValues;
+        function registerUser() {
+            userAccountService.registerUser(vm.userData).then(function (data) {
+                vm.isRegistered = true;
+            }, function (error, status) {
+                vm.isRegistered = false;
+                console.log(status);
+            });
+        }
+        function loginUser() {
+            userAccountService.loginUser(vm.userData).then(function (data) {
+                vm.isLoggedIn = true;
+                vm.userName = data.userName;
+                vm.bearerToken = data.access_token;
+            }, function (error, status) {
+                vm.isLoggedIn = false;
+                console.log(status);
+            });
+        }
+        function getValues() {
+            userAccountService.getValues().then(function (data) {
+                vm.values = data;
+                console.log('back... with success');
+            });
+        }
+    }
+})();
---
+++ NgSample/Scripts/auth/userAccountService.js
@@ -0,0 +1,77 @@
+﻿(function () {
+    'use strict';
+    var serviceId = 'userAccountService';
+    angular.module('app').factory(serviceId, ['$http', '$q', userAccountService]);
+    function userAccountService($http, $q) {
+        // Define the functions and properties to reveal.
+        var service = {
+            registerUser: registerUser,
+            loginUser: loginUser,
+            getValues: getValues,
+        };
+        var serverBaseUrl = "";
+
+        return service;
+        var accessToken = "";
+        function registerUser(userData) {
+            var accountUrl = serverBaseUrl + "/api/Account/Register";
+            var deferred = $q.defer();
+            $http({
+                method: 'POST',
+                url: accountUrl,
+                data: userData,
+            }).success(function (data, status, headers, cfg) {
+                console.log(data);
+                deferred.resolve(data);
+            }).error(function (err, status) {
+                console.log(err);
+                deferred.reject(status);
+            });
+            return deferred.promise;
+        }
+        function loginUser(userData) {
+            var tokenUrl = serverBaseUrl + "/Token";
+            if (!userData.grant_type) {
+                userData.grant_type = "password";
+            }
+            var deferred = $q.defer();
+            $http({
+                method: 'POST',
+                url: tokenUrl,
+                data: userData,
+            }).success(function (data, status, headers, cfg) {
+                // save the access_token as this is required for each API call. 
+                accessToken = data.access_token;
+                // check the log screen to know currently back from the server when a user log in successfully.
+                console.log(data);
+                deferred.resolve(data);
+            }).error(function (err, status) {
+                console.log(err);
+                deferred.reject(status);
+            });
+            return deferred.promise;
+        }
+        function getValues() {
+            var url = serverBaseUrl + "/api/values/";
+            var deferred = $q.defer();
+            $http({
+                method: 'GET',
+                url: url,
+                headers: getHeaders(),
+            }).success(function (data, status, headers, cfg) {
+                console.log(data);
+                deferred.resolve(data);
+            }).error(function (err, status) {
+                console.log(err);
+                deferred.reject(status);
+            });
+            return deferred.promise;
+        }
+        // we have to include the Bearer token with each call to the Web API controllers. 
+        function getHeaders() {
+            if (accessToken) {
+                return { "Authorization": "Bearer " + accessToken };
+            }
+        }
+    }
+})();
